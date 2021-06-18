fb53a3b3da7871b9b1db3d5b6478d262b7a11600
Sofia Lopez <anasolori.92@gmail.com>
2021-06-17 19:24:35 -0500

    Avances ejercicio cajero

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+let cantidadAtmMax = 20000;
+let billetesQuinientos = 20;
+let billetesDoscientos = 20;
+let billetesCien = 40;
+let billetesCincuenta = 40;
+
+const btnRetirar = document.querySelector('#retirar');
+let montoIngresado = 500;
+
+btnRetirar.addEventListener('click', function () {
+  alert('Funciono');
+});
+// console.log(montoIngresado);
+
+//function retiro() {}
